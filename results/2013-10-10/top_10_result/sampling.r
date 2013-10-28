c85eda04e205a46200eadff6c036e3db6fa041b4
chuan <zhouchuan121@gmail.com>
2013-10-28 15:24:29 +0800

    add top 1000 statistic to top 10 motif, collect all result to ods file

--- results/2013-10-10/top_10_result/sampling.r
+++ results/2013-10-10/top_10_result/sampling.r
@@ -3,7 +3,7 @@
 dd <- read.table("data.tsv", header=TRUE)
 ddCrs <- dd[!is.na(dd$raw_crs), ]
 #> dim(ddCrs)
-#[1] 2550345      11
+#[1] 2486865 7
 
 cmb_score <- read.table("hongwei_combined_score.tsv", header=TRUE)
 
@@ -13,42 +13,46 @@ n = 1000 # top n edges
 m = 1000 # sample times
 ccc <- replicate(m, countInRegulonEdge(ddCrs[sample(1:nrow(ddCrs), n), ]))
 (ccc)
-den<-density(unlist(ccc[1,]))
-hist(unlist(ccc[1,]), main="Regulon pairs in 1000 random pairs", xlab="Regulon pairs")
+data.frame(x = unlist(ccc[1,]), y = unlist(ccc[2,])) -> random_sample_count
+library(ggplot2)
+p + geom_density() + xlab("Regulon pairs") + labs(title = "Regulon pairs in 1000 random pairs")
+
+#den<-density(unlist(ccc[1,]))
+#hist(unlist(ccc[1,]), main="Regulon pairs in 1000 random pairs", xlab="Regulon pairs")
 
 topCrs <- countInRegulonEdge(ddCrs[ order(ddCrs$raw_crs, decreasing=TRUE)[1:n], ])
 #$inRegulonEdge
-#[1] 114
+#[1] 119
 #
 #$regulon
-#[1] 35
+#[1] 32
 
 top_similarity <- countInRegulonEdge(ddCrs[ order(ddCrs$similarity, decreasing=TRUE)[1:n], ])
 top_similarity
 #$inRegulonEdge
-#[1] 91
+#[1] 95
 #
 #$regulon
-#[1] 36
+#[1] 31
 
-
-ddpcs <- ddCrs[ !is.na(ddCrs$pcs_216) & !is.na(ddCrs$pcs_1286), ]
-topPcs_216 <- countInRegulonEdge(ddpcs[ order(ddpcs$pcs_216, decreasing=TRUE)[1:n], ])
-#$inRegulonEdge
-#[1] 22
-#
-#$regulon
-#[1] 13
-#
-topPcs_1286 <- countInRegulonEdge(ddpcs[ order(ddpcs$pcs_1286, decreasing=TRUE)[1:n], ])
-#$inRegulonEdge
-#[1] 36
-#
-#$regulon
-#[1] 21
+# pcs does not make a change, same as in top 30 motif
+#ddpcs <- ddCrs[ !is.na(ddCrs$pcs_216) & !is.na(ddCrs$pcs_1286), ]
+#topPcs_216 <- countInRegulonEdge(ddpcs[ order(ddpcs$pcs_216, decreasing=TRUE)[1:n], ])
+##$inRegulonEdge
+##[1] 22
+##
+##$regulon
+##[1] 13
+##
+#topPcs_1286 <- countInRegulonEdge(ddpcs[ order(ddpcs$pcs_1286, decreasing=TRUE)[1:n], ])
+##$inRegulonEdge
+##[1] 36
+##
+##$regulon
+##[1] 21
 
 ddCrs_merge_cmb_score <- merge(ddCrs, cmb_score, by=c("opr1", "opr2"))
-#> dim(ddCrs_merge_cmb_score)
+dim(ddCrs_merge_cmb_score)
 #[1] 638731     12
 top_cmb_score <- countInRegulonEdge(ddCrs_merge_cmb_score[ order(ddCrs_merge_cmb_score$combined_score, decreasing=TRUE)[1:n], ])
 #> top_cmb_score
@@ -62,7 +66,7 @@ top_cmb_score <- countInRegulonEdge(ddCrs_merge_cmb_score[ order(ddCrs_merge_cmb
 top_crs_with_cmb <- countInRegulonEdge(ddCrs_merge_cmb_score[ order(ddCrs_merge_cmb_score$raw_crs, decreasing=TRUE)[1:n], ])
 #> top_crs_with_cmb
 #$inRegulonEdge
-#[1] 139
+#[1] 144
 #
 #$regulon
 #[1] 33
@@ -70,7 +74,7 @@ top_crs_with_cmb <- countInRegulonEdge(ddCrs_merge_cmb_score[ order(ddCrs_merge_
 top_similarity_with_cmb <- countInRegulonEdge(ddCrs_merge_cmb_score[ order(ddCrs_merge_cmb_score$similarity, decreasing=TRUE)[1:n], ])
 top_similarity_with_cmb
 #$inRegulonEdge
-#[1] 102
+#[1] 104
 #
 #$regulon
 #[1] 26
@@ -83,7 +87,7 @@ countInRegulonEdge <- function(data) {
     d <- data[ data$in_regulon > 0, ]
     rv$inRegulonEdge <- nrow(d)
 
-    rv$regulon <- length(table(strsplit(paste(d$reg_names, collapse=''), '_'))) - 1
+    rv$regulon <- length(table(strsplit(paste(d$in_regulon_name, collapse=''), '_'))) - 1
 
     return(rv)
 }
