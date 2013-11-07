26341ca1312629c6da1a16e27f8920e03c5e0252
chuan <zhouchuan121@gmail.com>
2013-11-07 14:57:53 +0800

    cluster analysis added

--- results/2013-10-10/runall.pl
+++ results/2013-10-10/runall.pl
@@ -28,9 +28,17 @@ print $cmd;
 #system $cmd;
 $cmd = "python operon_graph_refine.py $output_d1/operon_table.tsv -1 -1 > $output_d1/operon_table_refined.tsv\n";
 print $cmd;
+#system $cmd;
+$cmd = "python operon_graph.py $output_d1/operon_table.tsv -1 -1 > $output_d1/operon_table_stat.tsv\n";
+print $cmd;
+#system $cmd;
+
+$cmd = "python motif_graph.py $output_d1/table.tsv -1 -1 > $output_d1/motif_graph_stat.tsv\n";
+print $cmd;
 system $cmd;
 
 
+print "\n-----------------------------------------\n";
 
 print "perl closurecompare2table.pl $data_d/$ccf2 $data_d/$motif_count_f2 > $output_d2/table.tsv\n";
 #system "perl closurecompare2table.pl $data_d/$ccf2 $data_d/$motif_count_f2 > $output_d2/table.tsv";
@@ -41,5 +49,12 @@ $cmd = "perl closurecompare2operontable.pl $data_d/$ccf2 $data_d/$motif_count_f2
 print $cmd;
 #system $cmd;
 $cmd = "python operon_graph_refine.py $output_d2/operon_table.tsv -1 -1 > $output_d2/operon_table_refined.tsv\n";
+print $cmd;
+#system $cmd;
+$cmd = "python operon_graph.py $output_d2/operon_table.tsv -1 -1 > $output_d2/operon_table_stat.tsv\n";
+print $cmd;
+#system $cmd;
+
+$cmd = "python motif_graph.py $output_d2/table.tsv -1 -1 > $output_d2/motif_graph_stat.tsv\n";
 print $cmd;
 system $cmd;
